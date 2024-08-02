eef50192ce2d67b4686df8c8107eec68de4d0c98
Michał Garapich <michal@garapich.pl>
2024-08-02 12:42:17 +0000

    test(e2e): cleanup

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test'
 import dotenv from 'dotenv'
 import type { AuthUsersOptions } from './tests/fixtures/auth-users'
 import { users } from './tests/data'
+import { minutesToMilliseconds } from 'date-fns'
 
 dotenv.config()
 
@@ -10,31 +11,24 @@ dotenv.config()
  */
 export default defineConfig<AuthUsersOptions>({
   testDir: './tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests */
   workers: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reportSlowTests: {
+    max: 5,
+    threshold: minutesToMilliseconds(2),
+  },
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-
     video: {
-      mode: 'on-first-retry',
+      mode: 'retain-on-failure',
       size: { width: 1280, height: 720 },
     },
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'setup database',
@@ -78,8 +72,6 @@ export default defineConfig<AuthUsersOptions>({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm dev',
     url: 'http://127.0.0.1:3000',
