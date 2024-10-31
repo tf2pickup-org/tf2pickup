9c5c30b71255378ec8bbb102f28cc36eea521cee
Michał Garapich <michal@garapich.pl>
2024-10-31 14:24:27 +0100

    feat: accept rules dialog (#43)

--- src/games/views/html/game-event-list.tsx
+++ src/games/views/html/game-event-list.tsx
@@ -27,7 +27,7 @@ export async function GameEventList(props: { game: GameModel }) {
 
   return (
     <div
-      class="game-event-list fade-scroll"
+      class="game-event-list fade-scroll h-[300px]"
       id={`game-${props.game.number}-event-list`}
       aria-label="Game events"
     >
