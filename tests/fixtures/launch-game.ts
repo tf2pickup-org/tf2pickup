3929dc517eeceda8363ce5b66832dcf37a8df8e1
Michał Garapich <michal@garapich.pl>
2024-11-07 09:55:32 +0100

    fix test

--- tests/fixtures/launch-game.ts
+++ tests/fixtures/launch-game.ts
@@ -6,15 +6,15 @@ export const launchGame = mergeTests(authUsers, simulateGameServer).extend<{
   gameNumber: number
 }>({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  gameNumber: async ({ users, steamIds, gameServer }, use) => {
+  gameNumber: async ({ users, gameServer }, use) => {
     if (users.count < 12) {
       throw new Error(`at least 12 users are required to launch a game`)
     }
 
-    const queueUsers = steamIds.slice(0, 12)
+    const queueUsers = users.getMany(12)
     await Promise.all(
       queueUsers
-        .map(steamId => users.bySteamId(steamId).queuePage())
+        .map(user => user.queuePage())
         .map(async (page, i) => {
           await page.slot(i).join()
           await page.readyUpDialog().readyUp()
