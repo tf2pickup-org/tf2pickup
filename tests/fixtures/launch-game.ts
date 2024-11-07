5a873725e1daf30f41fc1b3f22d4b294a805ec6b
Michał Garapich <michal@garapich.pl>
2024-11-07 10:36:35 +0100

    fix test

--- tests/fixtures/launch-game.ts
+++ tests/fixtures/launch-game.ts
@@ -11,12 +11,27 @@ export const launchGame = mergeTests(authUsers, simulateGameServer).extend<{
       throw new Error(`at least 12 users are required to launch a game`)
     }
 
+    const desiredSlots = new Map<string, number>([
+      ['Promenader', 0],
+      ['Mayflower', 1],
+      ['Polemic', 2],
+      ['Shadowhunter', 3],
+      ['MoonMan', 4],
+      ['Underfire', 5],
+      ['Astropower', 6],
+      ['LlamaDrama', 7],
+      ['SlitherTuft', 8],
+      ['Blacklight', 9],
+      ['AstraGirl', 10],
+      ['BellBoy', 11],
+    ])
+
     const queueUsers = users.getMany(12)
     await Promise.all(
       queueUsers
-        .map(user => user.queuePage())
-        .map(async (page, i) => {
-          await page.slot(i).join()
+        .map(user => ({ page: user.queuePage(), slot: desiredSlots.get(user.playerName)! }))
+        .map(async ({ page, slot }) => {
+          await page.slot(slot).join()
           await page.readyUpDialog().readyUp()
           await page.page.waitForURL(/games\/(\d+)/)
         }),
