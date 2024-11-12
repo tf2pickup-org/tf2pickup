0bda3403b3a087e539dc42e6eb19ef7c028657b3
Michał Garapich <michal@garapich.pl>
2024-11-12 15:28:14 +0000

    ci: skip auth tests

--- tests/00-misc/02-login.spec.ts
+++ tests/00-misc/02-login.spec.ts
@@ -1,10 +1,13 @@
-import { expect, test } from '@playwright/test'
+import { test } from '@playwright/test'
+
+test.skip(
+  () => !(process.env['STEAM_USERNAME'] && process.env['STEAM_PASSWORD']),
+  'STEAM_USERNAME and STEAM_PASSWORD are required to run this test',
+)
 
 test('login', async ({ page }) => {
-  const username = process.env['STEAM_USERNAME']
-  const password = process.env['STEAM_PASSWORD']
-  expect(username).toBeTruthy()
-  expect(password).toBeTruthy()
+  const username = process.env['STEAM_USERNAME']!
+  const password = process.env['STEAM_PASSWORD']!
 
   await page.goto('/')
   await page.getByRole('link', { name: 'Sign in through Steam' }).click()
@@ -17,9 +20,9 @@ test('login', async ({ page }) => {
     .locator('form')
     .filter({ hasText: 'Sign in with account' })
     .locator('input[type="text"]')
-    .fill(username!)
+    .fill(username)
   await page.locator('input[type="password"]').click()
-  await page.locator('input[type="password"]').fill(password!)
+  await page.locator('input[type="password"]').fill(password)
   await page.getByRole('button', { name: 'Sign in' }).click()
   await page.getByRole('button', { name: 'Sign In' }).click()
 })
