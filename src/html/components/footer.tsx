1c51818dd3a583eb7e2fcc47a0d278acca11a5f1
Michał Garapich <michal@garapich.pl>
2025-01-13 10:23:25 +0100

    fix: render correct copyright year (#126)

--- src/html/components/footer.tsx
+++ src/html/components/footer.tsx
@@ -1,12 +1,14 @@
 import type { User } from '../../auth/types/user'
 import { PlayerRole } from '../../database/models/player.model'
 
+const currentYear = new Date().getFullYear()
+
 export function Footer(props: { user?: User | undefined }) {
   return (
     <footer class="w-full">
       <div class="container mx-auto my-4">
-        <div class="text-abru-light-75 flex flex-col items-center gap-2 text-sm font-normal md:flex-row md:gap-0">
-          <span>© 2024 tf2pickup.org | version 12.0.0-alpha.0</span>
+        <div class="flex flex-col items-center gap-2 text-sm font-normal text-abru-light-75 md:flex-row md:gap-0">
+          <span>© 2019-{currentYear} tf2pickup.org | version 12.0.0-alpha.0</span>
           <div class="grow" />
           <div class="flex flex-col items-center gap-2 md:flex-row md:gap-5">
             <a href="/privacy-policy">Privacy policy</a>
