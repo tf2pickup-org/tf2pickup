49543c56ecd94062445c8f262fe3675dd72d9409
Michał Garapich <michal@garapich.pl>
2025-02-21 11:28:58 +0100

    refactor: cleanup twitch-tv module

--- src/twitch-tv/index.ts
+++ src/twitch-tv/index.ts
@@ -1,32 +1,9 @@
 import fp from 'fastify-plugin'
 import { environment } from '../environment'
 import { logger } from '../logger'
-import { Cron } from 'croner'
-import { getStreams } from './get-streams'
-import { events } from '../events'
-import { configuration } from '../configuration'
-import { collections } from '../database/collections'
-
-async function refreshStreams() {
-  const promotedStreams = await configuration.get('twitchtv.promoted_streams')
-  const twitchStreams = await getStreams({
-    userIds: [],
-    userLogins: promotedStreams,
-    type: 'live',
-  })
-  const streams = twitchStreams.map(stream => ({
-    id: stream.id,
-    userName: stream.user_name,
-    title: stream.title,
-    thumbnailUrl: stream.thumbnail_url,
-    viewerCount: stream.viewer_count,
-  }))
-  await collections.streams.deleteMany({})
-  if (streams.length > 0) {
-    await collections.streams.insertMany(streams)
-  }
-  events.emit('twitch.tv/streams:updated', { streams })
-}
+import { refreshStreams } from './refresh-streams'
+import { safe } from '../utils/safe'
+import { minutesToMilliseconds } from 'date-fns'
 
 export default fp(
   async () => {
@@ -40,14 +17,8 @@ export default fp(
       return
     }
 
-    new Cron('* * * * *', async () => {
-      try {
-        await refreshStreams()
-      } catch (error) {
-        logger.error(error)
-      }
-    })
-
+    setInterval(safe(refreshStreams), minutesToMilliseconds(1))
+    await refreshStreams()
     logger.info('twitch.tv integration enabled')
   },
   {
