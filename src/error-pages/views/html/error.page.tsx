0dc98320275a2644eecd253b9c69e4ebabb9c4bf
Michał Garapich <michal@garapich.pl>
2025-03-12 18:47:41 +0100

    fix: error pages overhaul (#167)

--- src/error-pages/views/html/error.page.tsx
+++ src/error-pages/views/html/error.page.tsx
@@ -5,15 +5,21 @@ import { Page } from '../../../html/components/page'
 import { Layout } from '../../../html/layout'
 import { makeTitle } from '../../../html/make-title'
 
-export function ErrorPage(props: { message: string; user?: User | undefined }) {
+export function ErrorPage(props: { statusCode: number; message: string; user?: User | undefined }) {
   return (
     <Layout title={makeTitle('Error')}>
       <NavigationBar user={props.user} />
       <Page>
-        <div class="flex h-full flex-row items-center justify-center">
-          <span class="my-9 text-[36px] font-bold text-rose-600" safe>
+        <div class="flex h-full flex-col items-center justify-center">
+          <span class="text-[384px] font-bold leading-none text-abru-light-75" safe>
+            {props.statusCode}
+          </span>
+          <span class="text-[36px] font-bold text-abru-light-75" safe>
             {props.message}
           </span>
+          <a href="/" class="button button--accent mt-4 px-16">
+            Go back home
+          </a>
         </div>
       </Page>
       <Footer user={props.user} />
