bd2d5acfc1eeb5f0d3e22be7a3f9d0e10b108a3e
Michał Garapich <michal@garapich.pl>
2025-03-15 15:26:44 +0100

    fix: set ws connect delay to 1 second (#177)

--- src/html/@client/main.ts
+++ src/html/@client/main.ts
@@ -1,12 +1,14 @@
-import './htmx'
+import htmx from './htmx'
+import 'htmx-ext-ws'
 import 'htmx.org/dist/ext/head-support.js'
-import 'htmx.org/dist/ext/ws.js'
 import 'htmx.org/dist/ext/remove-me.js'
 
 import * as hyperscript from 'hyperscript.org'
 hyperscript.browserInit()
 
 import './idiomorph.ts'
+
+htmx.config.wsReconnectDelay = () => 1000 // 1 second
 
 import './countdown'
 import './disable-when-offline'
