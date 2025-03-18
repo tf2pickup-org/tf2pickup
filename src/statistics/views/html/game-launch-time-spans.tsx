7e87faf9e6afdf4dbb86b98d92274ee6eb17f120
Michał Garapich <michal@garapich.pl>
2025-03-18 17:03:41 +0100

    init

---
+++ src/statistics/views/html/game-launch-time-spans.tsx
@@ -0,0 +1,5 @@
+import { getGameLaunchTimeSpans } from '../../get-game-launch-time-spans'
+
+export async function GameLaunchTimeSpans() {
+  const gameLaunchTimeSpans = await getGameLaunchTimeSpans()
+}
