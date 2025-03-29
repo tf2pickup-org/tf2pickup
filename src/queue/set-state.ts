b2e75f13cd631b569fbd3fa1c0bc0641fe19bf6d
Michał Garapich <michal@garapich.pl>
2025-03-29 19:20:26 +0100

    fix: don't ready up when switching classes as 11th player in the queue

--- src/queue/set-state.ts
+++ src/queue/set-state.ts
@@ -1,7 +1,9 @@
 import { collections } from '../database/collections'
 import { QueueState } from '../database/models/queue-state.model'
+import { errors } from '../errors'
 import { events } from '../events'
 import { logger } from '../logger'
+import { preReady } from '../pre-ready'
 import { mutex } from './mutex'
 
 export async function setState(state: QueueState) {
@@ -10,21 +12,26 @@ export async function setState(state: QueueState) {
     await collections.queueState.updateOne({}, { $set: { state } })
 
     if (state === QueueState.ready) {
+      const last = (await collections.queueState.findOne())?.last
+      if (!last) {
+        throw errors.internalServerError('invalid queue state: last undefined')
+      }
+
       const preReadiesPlayers = await collections.players
         .find({ preReadyUntil: { $gte: new Date() } })
         .toArray()
-      const toReadyUp = await collections.queueSlots
-        .find({ player: { $in: preReadiesPlayers.map(({ steamId }) => steamId) } })
-        .toArray()
+      const toReadyUp = (
+        await collections.queueSlots
+          .find({ player: { $in: preReadiesPlayers.map(({ steamId }) => steamId) } })
+          .toArray()
+      ).map(slot => slot.player!)
 
       const slots = (
         await Promise.all(
-          toReadyUp.map(
-            async slot =>
+          [...toReadyUp, last].map(
+            async player =>
               await collections.queueSlots.findOneAndUpdate(
-                {
-                  player: slot.player,
-                },
+                { player },
                 {
                   $set: { ready: true },
                 },
@@ -34,6 +41,7 @@ export async function setState(state: QueueState) {
         )
       ).filter(slot => slot !== null)
       events.emit('queue/slots:updated', { slots })
+      await preReady.start(last)
     }
 
     events.emit('queue/state:updated', { state })
