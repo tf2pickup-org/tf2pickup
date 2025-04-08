4de2f7e9792b5d30bcc88f1132aba1334c6a4132
Michał Garapich <michal@garapich.pl>
2025-04-08 10:31:20 +0200

    feat: add chat scroll (#218)

--- src/chat/get-snapshot.ts
+++ src/chat/get-snapshot.ts
@@ -1,9 +1,19 @@
+import type { Filter } from 'mongodb'
 import { collections } from '../database/collections'
 import type { ChatMessageModel } from '../database/models/chat-message.model'
+import { logger } from '../logger'
 
-export async function getSnapshot(params?: { limit?: number }): Promise<ChatMessageModel[]> {
+export async function getSnapshot(params?: {
+  limit?: number
+  before?: Date
+}): Promise<ChatMessageModel[]> {
+  logger.debug({ params }, `chat.getSnapshot()`)
   const limit = params?.limit ?? 20
-  return (
-    await collections.chatMessages.find({}, { sort: { at: -1 }, limit }).toArray()
-  ).toReversed()
+
+  let filter: Filter<ChatMessageModel> = {}
+  if (params?.before) {
+    filter = { at: { $lt: params.before } }
+  }
+
+  return await collections.chatMessages.find(filter, { sort: { at: -1 }, limit }).toArray()
 }
--- src/chat/routes.ts
+++ src/chat/routes.ts
@@ -2,28 +2,46 @@ import fp from 'fastify-plugin'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 import { send } from './send'
-import { ChatPrompt } from '../queue/views/html/chat'
+import { ChatMessageList, ChatPrompt } from '../queue/views/html/chat'
+import { getSnapshot } from './get-snapshot'
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export default fp(async app => {
-  app.withTypeProvider<ZodTypeProvider>().post(
-    '/chat',
-    {
-      config: {
-        authenticate: true,
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get(
+      '/chat',
+      {
+        schema: {
+          querystring: z.object({
+            before: z.string().transform(val => new Date(Number(val))),
+          }),
+        },
       },
-      schema: {
-        body: z.object({
-          message: z.string(),
-        }),
+      async (request, reply) => {
+        const { before } = request.query
+        const messages = await getSnapshot({ before })
+        return reply.status(200).send(await ChatMessageList({ messages }))
       },
-    },
-    async (request, reply) => {
-      await send({
-        author: request.user!.player.steamId,
-        body: request.body.message,
-      })
-      return reply.status(201).send(ChatPrompt())
-    },
-  )
+    )
+    .post(
+      '/chat',
+      {
+        config: {
+          authenticate: true,
+        },
+        schema: {
+          body: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+      async (request, reply) => {
+        await send({
+          author: request.user!.player.steamId,
+          body: request.body.message,
+        })
+        return reply.status(201).send(ChatPrompt())
+      },
+    )
 })
