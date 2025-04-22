59446f10a41b8ac4f4083679c04782c6249065d1
Michał Garapich <michal@garapich.pl>
2025-04-22 16:46:11 +0200

    test: disable firefox tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,38 +36,13 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'], ...(process.env.CI ? { ignoreHTTPSErrors: true } : {}) },
       dependencies: ['setup'],
     },
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-        ...(process.env.CI ? { ignoreHTTPSErrors: true } : {}),
-      },
-      dependencies: ['setup'],
-    },
-
     // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    //   name: 'firefox',
+    //   use: {
+    //     ...devices['Desktop Firefox'],
+    //     ...(process.env.CI ? { ignoreHTTPSErrors: true } : {}),
+    //   },
+    //   dependencies: ['setup'],
     // },
   ],
 })
