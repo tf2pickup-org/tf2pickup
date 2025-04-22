de6925c8cd632928c6a769b051dc41cbfd08534a
Michał Garapich <michal@garapich.pl>
2025-04-22 12:05:42 +0200

    test: configure games via the ui

--- tests/20-game/06-renders-join-game-button.spec.ts
+++ tests/20-game/06-renders-join-game-button.spec.ts
@@ -1,22 +1,9 @@
 import { secondsToMilliseconds } from 'date-fns'
-import { mergeTests } from '@playwright/test'
-import { accessMongoDb } from '../fixtures/access-mongo-db'
-import { launchGame, expect } from '../fixtures/launch-game'
+import { launchGame as test, expect } from '../fixtures/launch-game'
 
-const test = mergeTests(launchGame, accessMongoDb)
-
-test.beforeEach(async ({ db }) => {
-  const configuration = db.collection('configuration')
-  await configuration.updateOne(
-    { key: 'games.join_gameserver_timeout' },
-    { $set: { value: secondsToMilliseconds(30) } },
-    { upsert: true },
-  )
-  await configuration.updateOne(
-    { key: 'games.rejoin_gameserver_timeout' },
-    { $set: { value: secondsToMilliseconds(20) } },
-    { upsert: true },
-  )
+test.beforeEach(async ({ users }) => {
+  const page = await users.getAdmin().adminPage()
+  await page.configureGames({ joinGameServerTimeout: 30, rejoinGameServerTimeout: 20 })
 })
 
 test('renders join game button', async ({ gameNumber, users, gameServer }) => {
