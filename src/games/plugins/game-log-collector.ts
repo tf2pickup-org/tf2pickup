a2ca8ee37216cba9834b8e5342e5ae446eb57754
Michał Garapich <michal@garapich.pl>
2025-05-06 12:31:36 +0200

    fix: hide players IP address in game logs

--- src/games/plugins/game-log-collector.ts
+++ src/games/plugins/game-log-collector.ts
@@ -1,14 +1,16 @@
 import fp from 'fastify-plugin'
 import { events } from '../../events'
 import { collections } from '../../database/collections'
+import { hideIpAddresses } from '../../utils/hide-ip-addresses'
 
 export default fp(
   // eslint-disable-next-line @typescript-eslint/require-await
   async () => {
     events.on('gamelog:message', async ({ message }) => {
+      const logLine = hideIpAddresses(message.payload)
       await collections.gameLogs.findOneAndUpdate(
         { logSecret: message.password },
-        { $push: { logs: message.payload } },
+        { $push: { logs: logLine } },
         { upsert: true },
       )
     })
