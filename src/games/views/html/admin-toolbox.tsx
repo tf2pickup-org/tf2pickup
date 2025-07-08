d993359689a586f5d192ba583df2d30bdab9668e
Michał Garapich <michal@garapich.pl>
2025-07-08 18:23:08 +0200

    initial

---
+++ src/games/views/html/admin-toolbox.tsx
@@ -0,0 +1,5 @@
+import type { GameModel } from '../../../database/models/game.model'
+
+export function AdminToolbox(props: { game: GameModel }) {
+  return <div class="game-admin-toolbox"></div>
+}
