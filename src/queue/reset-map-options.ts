a242246b93a93b68e945e68aecc885e0591993d0
Michał Garapich <michal@garapich.pl>
2025-08-21 22:14:42 +0200

    fix: get rid of redundant swap

--- src/queue/reset-map-options.ts
+++ src/queue/reset-map-options.ts
@@ -1,7 +1,6 @@
 import { collections } from '../database/collections'
 import type { MapPoolEntry } from '../database/models/map-pool-entry.model'
 import { events } from '../events'
-import { getMapVoteResults } from './get-map-vote-results'
 import { mapPool } from './map-pool'
 
 export async function resetMapOptions() {
@@ -34,6 +33,4 @@ export async function resetMapOptions() {
   await collections.queueMapOptions.insertMany(choices.map(({ name }) => ({ name })))
   await collections.queueMapVotes.deleteMany({})
   events.emit('queue/mapOptions:reset', { mapOptions: choices.map(({ name }) => name) })
-  const results = await getMapVoteResults()
-  events.emit('queue/mapVoteResults:updated', { results })
 }
