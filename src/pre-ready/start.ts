fd74158a7c365eb0cd737af4b6237f2fc29f9b03
Michał Garapich <michal@garapich.pl>
2025-09-06 00:52:15 +0200

    more logs

--- src/pre-ready/start.ts
+++ src/pre-ready/start.ts
@@ -1,10 +1,13 @@
 import { configuration } from '../configuration'
+import { logger } from '../logger'
 import { players } from '../players'
 import type { SteamId64 } from '../shared/types/steam-id-64'
 
 export async function start(player: SteamId64) {
   const timeout = await configuration.get('queue.pre_ready_up_timeout')
   const preReadyUntil = new Date(Date.now() + timeout)
+
+  logger.trace({ timeout, preReadyUntil, player }, 'preReady.start()')
 
   if (timeout > 0) {
     await players.update(player, { $set: { preReadyUntil } })
