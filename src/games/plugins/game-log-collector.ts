2141be087a8a7115049e4a8fa8fe5d69fe7e36f0
Michał Garapich <michal@garapich.pl>
2025-09-29 22:04:19 +0200

    fix: handle failed gamelog push update

--- src/games/plugins/game-log-collector.ts
+++ src/games/plugins/game-log-collector.ts
@@ -2,18 +2,42 @@ import fp from 'fastify-plugin'
 import { events } from '../../events'
 import { collections } from '../../database/collections'
 import { hideIpAddresses } from '../../utils/hide-ip-addresses'
+import type { LogMessage } from '../../log-receiver/parse-log-message'
+import { MongoError } from 'mongodb'
 
 export default fp(
   // eslint-disable-next-line @typescript-eslint/require-await
   async () => {
     events.on('gamelog:message', async ({ message }) => {
-      const logLine = hideIpAddresses(message.payload)
+      await safePushLogMessage(message)
+    })
+  },
+  { name: 'game log collector', encapsulate: true },
+)
+
+async function safePushLogMessage(message: LogMessage) {
+  const logLine = hideIpAddresses(message.payload)
+
+  try {
+    await collections.gameLogs.findOneAndUpdate(
+      { logSecret: message.password },
+      { $push: { logs: logLine } },
+      { upsert: true },
+    )
+  } catch (error) {
+    if (!(error instanceof MongoError)) {
+      throw error
+    }
+
+    if (error.code === 11000) {
+      // Another upsert occurred during the upsert, try again.
       await collections.gameLogs.findOneAndUpdate(
         { logSecret: message.password },
         { $push: { logs: logLine } },
         { upsert: true },
       )
-    })
-  },
-  { name: 'game log collector', encapsulate: true },
-)
+    } else {
+      throw error
+    }
+  }
+}
