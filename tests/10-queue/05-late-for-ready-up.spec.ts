6c3461fa9182d780ffc1c3ba0958513be9494457
Michał Garapich <michal@garapich.pl>
2025-10-13 22:14:03 +0200

    test: fix late-for-ready-up test (#357)

--- tests/10-queue/05-late-for-ready-up.spec.ts
+++ tests/10-queue/05-late-for-ready-up.spec.ts
@@ -3,16 +3,22 @@ import { minutesToMilliseconds } from 'date-fns'
 
 test('player is late for ready up', async ({ players, desiredSlots, users, page }) => {
   test.setTimeout(minutesToMilliseconds(2))
+
+  const lateUserPage = await users.byName('MoonMan').queuePage()
+  await lateUserPage.goto()
+  await lateUserPage.slot(desiredSlots.get('MoonMan')!).join()
+
   await Promise.all(
-    players.map(async player => {
-      const page = await player.queuePage()
-      await page.goto()
-      const slot = desiredSlots.get(player.playerName)!
-      await page.slot(slot).join()
-    }),
+    players
+      .filter(player => player.playerName !== 'MoonMan')
+      .map(async player => {
+        const page = await player.queuePage()
+        await page.goto()
+        const slot = desiredSlots.get(player.playerName)!
+        await page.slot(slot).join()
+      }),
   )
 
-  const lateUser = users.byName('MoonMan')
   await Promise.all(
     players
       .filter(player => player.playerName !== 'MoonMan')
@@ -26,12 +32,12 @@ test('player is late for ready up', async ({ players, desiredSlots, users, page 
   await expect(page.getByRole('heading', { name: /^Players:/ })).toHaveText('Players: 11/12', {
     timeout: 60000,
   })
-  const kickedUserPage = await lateUser.queuePage()
-  await expect(kickedUserPage.slot('soldier-1').joinButton()).toBeVisible()
+
+  await expect(lateUserPage.slot('soldier-1').joinButton()).toBeVisible()
 
   // expect the ready up dialog to close
-  await expect(kickedUserPage.readyUpDialog().readyUpButton()).not.toBeVisible()
-  await expect(kickedUserPage.readyUpDialog().notReadyButton()).not.toBeVisible()
+  await expect(lateUserPage.readyUpDialog().readyUpButton()).not.toBeVisible()
+  await expect(lateUserPage.readyUpDialog().notReadyButton()).not.toBeVisible()
 
   // everybody leaves the queue
   await Promise.all(
