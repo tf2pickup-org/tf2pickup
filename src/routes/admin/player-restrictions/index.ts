c134eb85c120bb93ef70828287dcb7a654d033a0
Michał Garapich <michal@garapich.pl>
2025-11-01 20:19:20 +0100

    fix: configure default player skill

--- src/routes/admin/player-restrictions/index.ts
+++ src/routes/admin/player-restrictions/index.ts
@@ -4,6 +4,8 @@ import { z } from 'zod'
 import { configuration } from '../../../configuration'
 import { requestContext } from '@fastify/request-context'
 import { routes } from '../../../utils/routes'
+import { queue } from '../../../queue'
+import type { Tf2ClassName } from '../../../shared/types/tf2-class-name'
 
 const playerSkillThresholdSchema = z.discriminatedUnion('playerSkillThresholdEnabled', [
   z.object({
@@ -42,22 +44,41 @@ export default routes(async app => {
               etf2lAccountRequired: z.coerce.boolean().default(false),
               minimumInGameHours: z.coerce.number(),
               denyPlayersWithNoSkillAssigned: z.coerce.boolean().default(false),
+              ...queue.config.classes
+                .map(({ name }) => name)
+                .reduce<
+                  Partial<Record<`defaultPlayerSkill.${Tf2ClassName}`, z.ZodNumber>>
+                >((acc, key) => ({ ...acc, [`defaultPlayerSkill.${key}`]: z.coerce.number() }), {}),
             }),
           ),
         },
       },
       async (request, reply) => {
+        const {
+          etf2lAccountRequired,
+          minimumInGameHours,
+          denyPlayersWithNoSkillAssigned,
+          playerSkillThresholdEnabled,
+        } = request.body
+        const defaultPlayerSkill = Object.entries(request.body)
+          .filter(([key]) => key.startsWith('defaultPlayerSkill.'))
+          .reduce<Partial<Record<Tf2ClassName, number>>>(
+            (acc, [key, value]) => ({ ...acc, [key.split('.')[1] as Tf2ClassName]: value }),
+            {},
+          )
+
         await Promise.all([
-          configuration.set('players.etf2l_account_required', request.body.etf2lAccountRequired),
-          configuration.set('players.minimum_in_game_hours', request.body.minimumInGameHours),
+          configuration.set('players.etf2l_account_required', etf2lAccountRequired),
+          configuration.set('players.minimum_in_game_hours', minimumInGameHours),
           configuration.set(
             'queue.deny_players_with_no_skill_assigned',
-            request.body.denyPlayersWithNoSkillAssigned,
+            denyPlayersWithNoSkillAssigned,
           ),
           configuration.set(
             'queue.player_skill_threshold',
-            request.body.playerSkillThresholdEnabled ? request.body.playerSkillThreshold : null,
+            playerSkillThresholdEnabled ? request.body.playerSkillThreshold : null,
           ),
+          configuration.set('games.default_player_skill', defaultPlayerSkill),
         ])
         requestContext.set('messages', { success: ['Configuration saved'] })
         reply.status(200).html(await PlayerRestrictionsPage({ user: request.user! }))
