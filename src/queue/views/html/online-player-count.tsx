27a164e1ef44fdaa1cc711f1b5dfa7e76b34f1b1
Michał Garapich <michal@garapich.pl>
2025-11-19 16:38:35 +0100

    fix: hide online player list & chat in mobile views

--- src/queue/views/html/online-player-count.tsx
+++ src/queue/views/html/online-player-count.tsx
@@ -2,5 +2,9 @@ import { collections } from '../../../database/collections'
 
 export async function OnlinePlayerCount() {
   const onlinePlayers = await collections.onlinePlayers.countDocuments()
-  return <span id="online-player-count">Players online: {onlinePlayers}</span>
+  return (
+    <span id="online-player-count" class="whitespace-nowrap">
+      Players online: {onlinePlayers}
+    </span>
+  )
 }
