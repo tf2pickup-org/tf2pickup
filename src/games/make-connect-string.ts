13e3b659df9b1cc380cf5a0ad30cfbabfdea0bc5
Michał Garapich <michal@garapich.pl>
2025-11-30 22:31:56 +0100

    fix: resolve gameserver IP address for connect string

--- src/games/make-connect-string.ts
+++ src/games/make-connect-string.ts
@@ -1,14 +1,33 @@
+import { resolve4 } from 'dns/promises'
+import { isIP } from 'net'
+import { logger } from '../logger'
+
 interface MakeConnectStringProps {
   address: string
   port: string | number
   password: string | undefined
 }
 
-export function makeConnectString(props: MakeConnectStringProps) {
-  let connectString = `connect ${props.address}:${props.port}`
+export async function makeConnectString(props: MakeConnectStringProps) {
+  const address = await resolveAddress(props.address)
+  let connectString = `connect ${address}:${props.port}`
   if (props.password) {
     connectString += `; password ${props.password}`
   }
 
   return connectString
+}
+
+async function resolveAddress(address: string) {
+  if (isIP(address)) {
+    return address
+  }
+
+  try {
+    const addresses = await resolve4(address)
+    return addresses[0] ?? address
+  } catch (error) {
+    logger.error({ error, address }, `failed to resolve address`)
+    return address
+  }
 }
