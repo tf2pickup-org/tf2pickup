f129c9398334f9888b78cf8fc41541cdc720f7f1
Michał Garapich <michal@garapich.pl>
2025-12-01 18:58:21 +0100

    fix: fix updating player stats (#410)

--- src/games/plugins/update-player-stats.ts
+++ src/games/plugins/update-player-stats.ts
@@ -1,7 +1,6 @@
 import fp from 'fastify-plugin'
 import { events } from '../../events'
 import { GameState } from '../../database/models/game.model'
-import { SlotStatus } from '../../database/models/game-slot.model'
 import { players } from '../../players'
 import { safe } from '../../utils/safe'
 
@@ -18,20 +17,14 @@ export default fp(
 
         // Update stats for each player slot that was active
         await Promise.all(
-          game.slots
-            .filter(slot => slot.status === SlotStatus.active)
-            .map(async slot => {
-              await players.update(slot.player, {
-                $setOnInsert: {
-                  'stats.totalGames': 0,
-                  'stats.gamesByClass': {},
-                },
-                $inc: {
-                  'stats.totalGames': 1,
-                  [`stats.gamesByClass.${slot.gameClass}`]: 1,
-                },
-              })
-            }),
+          game.slots.map(async slot => {
+            await players.update(slot.player, {
+              $inc: {
+                'stats.totalGames': 1,
+                [`stats.gamesByClass.${slot.gameClass}`]: 1,
+              },
+            })
+          }),
         )
       }),
     )
