4c4a33a2c265b73b8136981e4413da89f3cfc036
Michał Garapich <michal@garapich.pl>
2025-12-07 22:39:04 +0100

    fix: update player avatar on sign in (#423)

--- src/players/types/create-player-params.ts
+++
@@ -1,9 +0,0 @@
-import type { PlayerAvatar, PlayerRole } from '../../database/models/player.model'
-import type { SteamId64 } from '../../shared/types/steam-id-64'
-
-export interface CreatePlayerParams {
-  steamId: SteamId64
-  name: string
-  avatar: PlayerAvatar
-  roles?: PlayerRole[]
-}
