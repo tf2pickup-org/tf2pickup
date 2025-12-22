fe616798fa5b90a29ccd76bc1653dd647fc4dcb3
Michał Garapich <garrappachc@gmail.com>
2025-12-22 18:29:33 +0100

    use players.update()

--- src/players/set-skill.ts
+++ src/players/set-skill.ts
@@ -2,6 +2,7 @@ import { collections } from '../database/collections'
 import { GameState, type GameNumber } from '../database/models/game.model'
 import type { PlayerSkill } from '../database/models/player.model'
 import type { SteamId64 } from '../shared/types/steam-id-64'
+import { update } from './update'
 
 interface SetSkillParams {
   steamId: SteamId64
@@ -11,20 +12,17 @@ interface SetSkillParams {
 
 export async function setSkill({ steamId, skill, actor }: SetSkillParams) {
   const lastGame = await getLastGameNumber()
-  await collections.players.updateOne(
-    { steamId },
-    {
-      $set: { skill },
-      $push: {
-        skillHistory: {
-          at: new Date(),
-          skill,
-          actor,
-          lastGame,
-        },
+  await update(steamId, {
+    $set: { skill },
+    $push: {
+      skillHistory: {
+        at: new Date(),
+        skill,
+        actor,
+        lastGame,
       },
     },
-  )
+  })
 }
 
 async function getLastGameNumber(): Promise<GameNumber | undefined> {
