208122ba943238a911c8db9079f5e9122049fbd3
Michał Garapich <michal@garapich.pl>
2025-12-23 23:07:01 +0100

    fix: register task processor in onReady hook (#449)

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -2,7 +2,6 @@ import { register } from './register'
 import { schedule } from './schedule'
 import { cancelAll } from './cancel-all'
 import { cancel } from './cancel'
-import './process'
 
 export const tasks = {
   cancelAll,
