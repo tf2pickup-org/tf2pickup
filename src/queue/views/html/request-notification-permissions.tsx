47901eecce1b12886f18e38cd158d25e9d70f4db
Michał Garapich <michal@garapich.pl>
2025-12-27 10:57:59 +0100

    feat: add dismiss notifications disabled banner button

--- src/queue/views/html/request-notification-permissions.tsx
+++ src/queue/views/html/request-notification-permissions.tsx
@@ -1,5 +1,6 @@
 import { resolve } from 'node:path'
 import { bundle } from '../../../html/bundle'
+import { IconX } from '../../../html/components/icons'
 
 export async function RequestNotificationPermissions() {
   const scriptJs = await bundle(
@@ -19,21 +20,29 @@ export async function RequestNotificationPermissions() {
         </div>
       </div>
       <div id="notifications-permission-denied" style="display: none;">
-        <div class="banner banner--warning">
+        <div class="banner banner--warning flex flex-row items-start gap-x-3">
           <p class="flex-1">
             You have disabled browser notifications. You will not be warned when a game is about to
             start.
           </p>
+          <button
+            class="self-center opacity-70 transition-opacity hover:opacity-100"
+            id="dismiss-notifications-banner"
+            aria-label="Dismiss"
+          >
+            <IconX size={24} />
+          </button>
         </div>
       </div>
       <script type="module">{`
         import { requestNotificationPermissions } from '${scriptJs}';
 
         const button = document.getElementById('request-notifications-permission');
+        const dismissButton = document.getElementById('dismiss-notifications-banner');
         const bannerDefault = document.getElementById('notifications-permission-default');
         const bannerDenied = document.getElementById('notifications-permission-denied');
 
-        requestNotificationPermissions({ button, bannerDefault, bannerDenied });
+        requestNotificationPermissions({ button, dismissButton, bannerDefault, bannerDenied });
       `}</script>
     </>
   )
