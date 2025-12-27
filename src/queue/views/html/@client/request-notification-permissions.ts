c3c8b9bb487d51c40c6a9c9ed64960497fa0c0a3
Michał Garapich <michal@garapich.pl>
2025-12-27 11:07:29 +0100

    feat: add dismiss notifications disabled banner button (#456)

--- src/queue/views/html/@client/request-notification-permissions.ts
+++ src/queue/views/html/@client/request-notification-permissions.ts
@@ -1,21 +1,31 @@
 interface NotificationPermissionsParams {
   button: HTMLButtonElement
+  dismissButton: HTMLButtonElement
   bannerDefault: HTMLElement
   bannerDenied: HTMLElement
 }
 
+const dismissedBannerKey = 'notifications-banner-dismissed'
+
 export function requestNotificationPermissions(params: NotificationPermissionsParams) {
+  const isDismissed = localStorage.getItem(dismissedBannerKey) === 'true'
+
   if (Notification.permission === 'default') {
     params.bannerDefault.style.display = 'block'
 
     params.button.addEventListener('click', async () => {
       const permission = await Notification.requestPermission()
       params.bannerDefault.style.display = 'none'
-      if (permission === 'denied') {
+      if (permission === 'denied' && !isDismissed) {
         params.bannerDenied.style.display = 'block'
       }
     })
-  } else if (Notification.permission === 'denied') {
+  } else if (Notification.permission === 'denied' && !isDismissed) {
     params.bannerDenied.style.display = 'block'
   }
+
+  params.dismissButton.addEventListener('click', () => {
+    params.bannerDenied.style.display = 'none'
+    localStorage.setItem(dismissedBannerKey, 'true')
+  })
 }
