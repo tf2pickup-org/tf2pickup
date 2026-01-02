71f8bbe16dc19e4f12c25d4551f4b69bef293f5a
Michał Garapich <garrappachc@gmail.com>
2026-01-02 13:47:02 +0100

    refactor: implement sound playback using custom events

--- src/games/views/html/server-ready-notification.tsx
+++ src/games/views/html/server-ready-notification.tsx
@@ -1,14 +1,18 @@
+import { nanoid } from 'nanoid'
 import { players } from '../../../players'
 import type { SteamId64 } from '../../../shared/types/steam-id-64'
 
 export async function ServerReadyNotification(actor: SteamId64) {
   const player = await players.bySteamId(actor, ['preferences.soundVolume'])
+  const id = nanoid()
   return (
     <div id="notify-container" hx-swap-oob="beforeend">
       <div
+        id={id}
         play-sound-src="/sounds/fight.webm"
         play-sound-volume={player.preferences.soundVolume ?? '1.0'}
       ></div>
+      <script>{`document.getElementById('${id}').dispatchEvent(new CustomEvent('tf2pickup:soundPlay'))`}</script>
     </div>
   )
 }
