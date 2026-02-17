238b6fa9bd551236b4ec7f5a28db6bb40cc68cd0
Michał Garapich <michal@garapich.pl>
2026-02-17 19:03:58 +0100

    remove old configs

--- src/queue/configs/index.ts
+++ src/queue/configs/index.ts
@@ -1,15 +1,9 @@
 import { _6v6 } from './6v6'
-import { test } from './test'
 import { _9v9 } from './9v9'
-import { bball } from './bball'
-import { ultiduo } from './ultiduo'
 import type { QueueConfig } from '../types/queue-config'
 import { environment } from '../../environment'
 
 export const queueConfigs: Record<typeof environment.QUEUE_CONFIG, QueueConfig> = {
   '6v6': _6v6,
   '9v9': _9v9,
-  bball,
-  test,
-  ultiduo,
 }
