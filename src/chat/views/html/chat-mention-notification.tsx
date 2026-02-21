a04027c9f2980c0f06103d4cbd5ff8134cf544f7
Michał Garapich <michal@garapich.pl>
2026-02-21 22:17:27 +0100

    feat(chat): add mention notification fragment

---
+++ src/chat/views/html/chat-mention-notification.tsx
@@ -0,0 +1,15 @@
+import { players } from '../../../players'
+import type { SteamId64 } from '../../../shared/types/steam-id-64'
+
+export async function ChatMentionNotification(actor: SteamId64) {
+  const player = await players.bySteamId(actor, ['preferences.soundVolume'])
+  return (
+    <div id="notify-container" hx-swap-oob="beforeend">
+      <div
+        play-sound-src="/sounds/mention.webm"
+        play-sound-volume={player.preferences.soundVolume ?? '1.0'}
+      ></div>
+      <script type="module" remove-me="0s">{`document.dispatchEvent(new CustomEvent('chat:mentioned'));`}</script>
+    </div>
+  )
+}
