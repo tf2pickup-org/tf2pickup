38a10371449f06ae80cc50548ef6557788c95b18
Michał Garapich <garrappachc@gmail.com>
2026-02-25 16:20:11 +0100

    fix: reduce queue cooldown for replacement players

--- src/games/plugins/free-players.ts
+++ src/games/plugins/free-players.ts
@@ -3,6 +3,7 @@ import { tasks } from '../../tasks'
 import { players } from '../../players'
 import { configuration } from '../../configuration'
 import { events } from '../../events'
+import { GameEventType, type PlayerReplaced } from '../../database/models/game-event.model'
 
 export default fp(
   // eslint-disable-next-line @typescript-eslint/require-await
@@ -12,10 +13,19 @@ export default fp(
     })
 
     events.on('game:ended', async ({ game }) => {
+      const substitutes = new Set(
+        game.events
+          .filter(
+            (e): e is PlayerReplaced =>
+              e.event === GameEventType.playerReplaced && e.replacee !== e.replacement,
+          )
+          .map(({ replacement }) => replacement),
+      )
+
       await Promise.all(
         game.slots.map(async ({ gameClass, player }) => {
           const queueCooldown = await configuration.get('games.join_queue_cooldown')
-          const cooldownMs = queueCooldown[gameClass]
+          const cooldownMs = substitutes.has(player) ? 0 : queueCooldown[gameClass]
           await tasks.schedule('games.freePlayer', cooldownMs, { player })
         }),
       )
