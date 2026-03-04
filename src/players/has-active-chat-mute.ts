c5eace7e0c7cba3925d9ef60c17ea57f4a6537b3
Michał Garapich <michal@garapich.pl>
2026-03-04 17:01:01 +0100

    style: format hasActiveChatMute signature

--- src/players/has-active-chat-mute.ts
+++ src/players/has-active-chat-mute.ts
@@ -1,7 +1,5 @@
 import type { PlayerModel } from '../database/models/player.model'
 
-export function hasActiveChatMute(
-  player: Pick<PlayerModel, 'chatMutes'>,
-): boolean {
+export function hasActiveChatMute(player: Pick<PlayerModel, 'chatMutes'>): boolean {
   return (player.chatMutes ?? []).some(mute => mute.end > new Date())
 }
