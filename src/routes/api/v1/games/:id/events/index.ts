4ac11db1f8f1e1765b12dc01ae3fcf28b123c04d
Michał Garapich <michal@garapich.pl>
2026-03-05 13:02:05 +0100

    format

--- src/routes/api/v1/games/:id/events/index.ts
+++ src/routes/api/v1/games/:id/events/index.ts
@@ -15,9 +15,7 @@ export default routes(async app => {
     },
     async (request, reply) => {
       const game = await games.findOne({ number: request.params.id })
-      const events = game.events
-        .map(gameEventToPublicDto)
-        .filter(isNotNil)
+      const events = game.events.map(gameEventToPublicDto).filter(isNotNil)
 
       return reply
         .type('application/hal+json')
