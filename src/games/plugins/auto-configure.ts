5e0717fd1bd26d4bef6d571a950f3a7e59d9c3cd
Michał Garapich <michal@garapich.pl>
2026-03-27 16:58:46 +0100

    fix: extend configure timeout for quickserver servers

--- src/games/plugins/auto-configure.ts
+++ src/games/plugins/auto-configure.ts
@@ -2,10 +2,15 @@ import fp from 'fastify-plugin'
 import { logger } from '../../logger'
 import { events } from '../../events'
 import { configure } from '../rcon/configure'
-import { GameState, type GameModel, type GameNumber } from '../../database/models/game.model'
+import {
+  GameServerProvider,
+  GameState,
+  type GameModel,
+  type GameNumber,
+} from '../../database/models/game.model'
 import { GameEventType } from '../../database/models/game-event.model'
 import { update } from '../update'
-import { minutesToMilliseconds } from 'date-fns'
+import { minutesToMilliseconds, secondsToMilliseconds } from 'date-fns'
 import { configuration } from '../../configuration'
 
 export default fp(
@@ -17,10 +22,7 @@ export default fp(
       try {
         configurators.get(game.number)?.abort()
         const controller = new AbortController()
-        const timeoutMs = game.gameServer?.pendingTaskId
-          ? (await configuration.get('tf2_quick_server.timeout')) + minutesToMilliseconds(1)
-          : minutesToMilliseconds(1)
-        const timeout = AbortSignal.timeout(timeoutMs)
+        const timeout = AbortSignal.timeout(await configureTimeout(game))
         const signal = AbortSignal.any([controller.signal, timeout])
         const configurator = configure(game, { signal })
         configurators.set(game.number, controller)
@@ -69,3 +71,19 @@ export default fp(
     encapsulate: true,
   },
 )
+
+async function configureTimeout(game: GameModel) {
+  const configureRconTimeout = minutesToMilliseconds(1)
+
+  if (game.gameServer?.pendingTaskId) {
+    // waiting for a new quick server to start
+    return (await configuration.get('tf2_quick_server.timeout')) + configureRconTimeout
+  }
+
+  if (game.gameServer?.provider === GameServerProvider.tf2QuickServer) {
+    // 90 seconds for STV timeout
+    return secondsToMilliseconds(90) + configureRconTimeout
+  }
+
+  return configureRconTimeout
+}
