1d49f34113294b6b66dd0006d42ba91d93499270
Michał Garapich <michal@garapich.pl>
2026-04-16 21:39:28 +0200

    fix

--- src/html/components/footer.tsx
+++ src/html/components/footer.tsx
@@ -32,7 +32,13 @@ export function Footer() {
             </a>
 
             {user?.player.roles.includes(PlayerRole.admin) && <a href="/admin">Admin panel</a>}
-            <div id="ws-status" data-ws-status="connecting" role="status" aria-label="Connecting...">
+            <div
+              id="ws-status"
+              data-ws-status="connecting"
+              role="status"
+              aria-label="Connecting..."
+              hx-preserve
+            >
               <span class="ball" />
               <span class="tooltip whitespace-nowrap">Connecting...</span>
             </div>
