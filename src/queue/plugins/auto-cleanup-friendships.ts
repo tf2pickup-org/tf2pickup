792c32e708db2620a49707547c849eb3be802f73
Michał Garapich <michal@garapich.pl>
2026-05-19 14:23:20 +0200

    clenaup

--- src/queue/plugins/auto-cleanup-friendships.ts
+++ src/queue/plugins/auto-cleanup-friendships.ts
@@ -1,18 +1,12 @@
 import fp from 'fastify-plugin'
 import { events } from '../../events'
 import { cleanupFriendships } from '../cleanup-friendships'
-import { logger } from '../../logger'
+import { debounce } from 'es-toolkit'
 
 export default fp(
   // eslint-disable-next-line @typescript-eslint/require-await
   async () => {
-    let debounceTimer: NodeJS.Timeout | undefined
-    events.on('queue/slots:updated', () => {
-      clearTimeout(debounceTimer)
-      debounceTimer = setTimeout(() => {
-        cleanupFriendships().catch(error => logger.error(error))
-      }, 100)
-    })
+    events.on('queue/slots:updated', debounce(cleanupFriendships, 100))
   },
   {
     name: 'auto cleanup friendships',
