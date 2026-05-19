3b6a4314ee782c1c065ba1acbe32b0bf0bb56c45
Michał Garapich <garrappachc@gmail.com>
2026-05-19 14:18:58 +0200

    fix: cleanupFriendships() performance improvements

--- src/queue/plugins/auto-cleanup-friendships.ts
+++ src/queue/plugins/auto-cleanup-friendships.ts
@@ -1,12 +1,17 @@
 import fp from 'fastify-plugin'
 import { events } from '../../events'
 import { cleanupFriendships } from '../cleanup-friendships'
+import { logger } from '../../logger'
 
 export default fp(
   // eslint-disable-next-line @typescript-eslint/require-await
   async () => {
-    events.on('queue/slots:updated', async () => {
-      await cleanupFriendships()
+    let debounceTimer: NodeJS.Timeout | undefined
+    events.on('queue/slots:updated', () => {
+      clearTimeout(debounceTimer)
+      debounceTimer = setTimeout(() => {
+        cleanupFriendships().catch(error => logger.error(error))
+      }, 100)
     })
   },
   {
