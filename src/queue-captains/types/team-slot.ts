4e6e9ce2a407f6949c374861c8a88c4687ef9bab
Michał Garapich <michal@garapich.pl>
2026-08-11 11:39:00 +0200

    Revert "feat(queue): add the captain draft matching engine (#765)"
    This reverts commit e216315e012f385c0b0e56c9c304f4cd74d00cf1.

--- src/queue-captains/types/team-slot.ts
+++
@@ -1,7 +0,0 @@
-import type { Tf2ClassName } from '../../shared/types/tf2-class-name'
-import type { Tf2Team } from '../../shared/types/tf2-team'
-
-export interface TeamSlot {
-  team: Tf2Team
-  gameClass: Tf2ClassName
-}
